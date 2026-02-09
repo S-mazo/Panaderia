03daa96ea7af4d27f43ee47afaf097c3070cc083
Firstonefire <213894236+S-mazo@users.noreply.github.com>
2026-02-09 09:28:53 -0500

    netlify ajustes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // Habilitar exportación estática para GitHub Pages
-  basePath: '/Panaderia',  // Necesario porque el repo no está en la raíz del dominio
+  output: 'export',  // Habilitar exportación estática
   typescript: {
     ignoreBuildErrors: true,
   },
